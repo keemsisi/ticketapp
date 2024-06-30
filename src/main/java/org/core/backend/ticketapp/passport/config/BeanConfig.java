fa40d0e73af0e8a77293174edc93d7c40714fdfe
Adeshina <91054814+integral1@users.noreply.github.com>
2024-06-30 17:09:43 +0100

    updated codes

--- src/main/java/org/core/backend/ticketapp/passport/config/BeanConfig.java
+++ src/main/java/org/core/backend/ticketapp/passport/config/BeanConfig.java
@@ -2,22 +2,56 @@ package org.core.backend.ticketapp.passport.config;
 
 
 import org.core.backend.ticketapp.passport.util.SharedEnvironment;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class BeanConfig {
 
+    private static final String CLASS_PATH = "classpath:db/migration";
     SharedEnvironment sharedEnvironment;
     @Autowired
     private Environment env;
+    @Value(value = "${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value(value = "${spring.datasource.url}")
+    private String databaseUrl;
+    @Value(value = "${spring.datasource.username}")
+    private String username;
+    @Value(value = "${spring.datasource.password}")
+    private String password;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(4);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(databaseUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+    @Bean(initMethod = "migrate")
+    public Flyway flyway() {
+        final Flyway flyway = Flyway.configure()
+                .dataSource(dataSource()).locations(CLASS_PATH)
+                .baselineOnMigrate(true).load();
+        flyway.repair();
+        flyway.migrate();
+        return flyway;
     }
 }
