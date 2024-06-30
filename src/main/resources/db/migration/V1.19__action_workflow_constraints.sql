5071842a4bcb87c16eaab623794c430de723f307
Adeshina <91054814+integral1@users.noreply.github.com>
2024-06-30 05:00:34 +0100

    update on codes

--- src/main/resources/db/migration/V1.19__action_workflow_constraints.sql
+++ src/main/resources/db/migration/V1.19__action_workflow_constraints.sql
@@ -2,12 +2,12 @@ ALTER TABLE action_workflow
 DROP CONSTRAINT IF EXISTS tbl_workflow_col_workflow_id_fkey;
 
 ALTER TABLE action_workflow
-ADD CONSTRAINT tbl_workflow_col_workflow_id_fkey FOREIGN KEY (workflow_id)
+ADD CONSTRAINT IF NOT EXISTS tbl_workflow_col_workflow_id_fkey FOREIGN KEY (workflow_id)
 REFERENCES workflow(id);
 
 ALTER TABLE action_workflow
 DROP CONSTRAINT IF EXISTS tbl_action_col_action_id_fkey;
 
 ALTER TABLE action_workflow
-ADD CONSTRAINT tbl_action_col_action_id_fkey FOREIGN KEY (action_id)
+ADD CONSTRAINT IF NOT EXISTS tbl_action_col_action_id_fkey FOREIGN KEY (action_id)
 REFERENCES action(id);
