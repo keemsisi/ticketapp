4902a82f82fc774f8f1aa1f5a66e3dc3339a73fd
Adeshina <91054814+integral1@users.noreply.github.com>
2024-07-08 23:57:05 +0100

    added more filter query param to events

--- src/main/java/org/core/backend/ticketapp/common/request/events/EventFilterRequestDTO.java
+++ src/main/java/org/core/backend/ticketapp/common/request/events/EventFilterRequestDTO.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.core.backend.ticketapp.common.enums.ApprovalStatus;
+import org.core.backend.ticketapp.common.enums.EventCategoryEnum;
 import org.core.backend.ticketapp.common.request.AbstractFilterRequestDTO;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotNull;
 import java.util.Date;
+import java.util.UUID;
+
+enum EventType {
+    PHYSICAL, FREE, PAID, VIRTUAL
+}
 
 @AllArgsConstructor
 @Getter
@@ -18,4 +24,17 @@ public class EventFilterRequestDTO extends AbstractFilterRequestDTO {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date startDateApproved;
     private ApprovalStatus approvalStatus;
+    private EventCategoryEnum eventCategoryEnum;
+    private UUID seatSection;
+    private double startPrice;
+    private double endPrice;
+    private String address;
+    private String state;
+    private String country;
+    private String artistName;
+    private String eventType;
+    private Boolean isPaidEvent;
+    private Boolean isPhysicalEvent;
+    private String title;
+    private String description;
 }
