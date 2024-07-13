9b71b45ac8c0d008689d91bb0523449591bdb3b5
Adeshina <91054814+integral1@users.noreply.github.com>
2024-07-13 02:59:00 +0100

    updated codes

--- src/main/java/org/core/backend/ticketapp/TicketApp.java
+++ src/main/java/org/core/backend/ticketapp/TicketApp.java
@@ -7,13 +7,12 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = RedisAutoConfiguration.class)
+@EnableAutoConfiguration()
 @EnableAsync
 @EnableFeignClients
 public class TicketApp {
--- src/main/java/org/core/backend/ticketapp/passport/socketio/config/NotificationSocketIOConfiguration.java
+++ src/main/java/org/core/backend/ticketapp/passport/socketio/config/NotificationSocketIOConfiguration.java
@@ -15,22 +15,22 @@ public class NotificationSocketIOConfiguration {
     private Integer port;
     @Value(value = "${server.netty-socket-origin}")
     private String origin;
-//    @Autowired
-//    private RedissonClient redissonClient;
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Bean
     public SocketIOServer socketIOServer() {
-//        RedissonStoreFactory redisStoreFactory = new RedissonStoreFactory(redissonClient);
+        RedissonStoreFactory redisStoreFactory = new RedissonStoreFactory(redissonClient);
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-//        config.setStoreFactory(redisStoreFactory);
+        config.setStoreFactory(redisStoreFactory);
         config.setPort(port);
         config.setOrigin(origin);
         return new SocketIOServer(config);
     }
-//
-//    @Bean
-//    public SpringAnnotationScanner springAnnotationScanner() {
-//        return new SpringAnnotationScanner(socketIOServer());
-//    }
+
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner() {
+        return new SpringAnnotationScanner(socketIOServer());
+    }
 }
 
