cd3c3a8a285ef1cc078ccffeb052ef6f23214491
Lasisi Adeshina <keemsisi@gmail.com>
2024-08-15 23:33:47 +0100

    feat/transaction-api

--- src/main/java/org/core/backend/ticketapp/subscription/controller/SubscriptionController.java
+++ src/main/java/org/core/backend/ticketapp/subscription/controller/SubscriptionController.java
@@ -16,26 +16,26 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/orders")
+@RequestMapping("/api/v1/subscription")
 public class SubscriptionController implements ICrudController {
 
-    SubscriptionService orderService;
+    SubscriptionService subscriptionService;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GenericResponse<List<Subscription>>> getAll() throws Exception {
-        final var orders = orderService.getAll();
-        return new ResponseEntity<>(new GenericResponse<>("00", "All orders", orders), HttpStatus.OK);
+        final var subscriptions = subscriptionService.getAll();
+        return new ResponseEntity<>(new GenericResponse<>("00", "All subscriptions", subscriptions), HttpStatus.OK);
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GenericResponse<Subscription>> getById(@PathVariable UUID id) {
-        final var data = orderService.getById(id);
-        return ResponseEntity.ok(new GenericResponse<>("00", "Fetched successfully", data));
+        final var subscription = subscriptionService.getById(id);
+        return ResponseEntity.ok(new GenericResponse<>("00", "Fetched successfully", subscription));
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> delete(@PathVariable UUID id) {
-        orderService.delete(id);
+        subscriptionService.delete(id);
         return ResponseEntity.ok(new GenericResponse<>("00", "Deleted successfully", null));
     }
 }
