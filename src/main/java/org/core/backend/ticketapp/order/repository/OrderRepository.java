6fb114256b7fa0390d36f7e6e58942dc4136c9b1
Adeshina <91054814+integral1@users.noreply.github.com>
2024-09-01 19:39:46 +0100

    updated codes

--- src/main/java/org/core/backend/ticketapp/order/repository/OrderRepository.java
+++ src/main/java/org/core/backend/ticketapp/order/repository/OrderRepository.java
@@ -5,10 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    @Query(value = "SELECT * FROM order WHERE batch_id = ?1 AND deleted=false")
+    @Query(value = "SELECT * FROM order WHERE batch_id = ?1 AND deleted=false", nativeQuery = true)
     List<Order> findByBatchId(final UUID id);
 }
