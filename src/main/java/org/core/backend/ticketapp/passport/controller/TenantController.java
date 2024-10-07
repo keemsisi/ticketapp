c12a0a8670331c900a080559e82773fed41519b4
Adeshina <91054814+integral1@users.noreply.github.com>
2024-10-07 10:02:47 +0100

    added operating hours and bank account details

--- src/main/java/org/core/backend/ticketapp/passport/controller/TenantController.java
+++ src/main/java/org/core/backend/ticketapp/passport/controller/TenantController.java
@@ -143,7 +143,7 @@ public class TenantController {
     }
 
     @RequestMapping(value = "/bank-account-details", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GenericResponse<List<BankAccountDetails>>> getAccountDetails(@RequestParam(required = false) UUID tenantId) {
+    public ResponseEntity<GenericResponse<List<BankAccountDetails>>> getBankAccountDetails(@RequestParam(required = false) UUID tenantId) {
         final var user = jwtTokenUtil.getUser();
         if (Objects.nonNull(tenantId)) {
             UserUtils.assertUserHasRole(user.getRoles(), AccountType.SUPER_ADMIN.getType());
