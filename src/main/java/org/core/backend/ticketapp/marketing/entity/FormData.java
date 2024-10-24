a80abc6532726c9d3155e9040db3824a49969333
Adeshina <91054814+integral1@users.noreply.github.com>
2024-10-24 15:34:07 +0100

    form data updates

--- src/main/java/org/core/backend/ticketapp/marketing/entity/FormData.java
+++ src/main/java/org/core/backend/ticketapp/marketing/entity/FormData.java
@@ -27,11 +27,14 @@ public class FormData extends AbstractBaseEntity {
     private String title;
     @Column(name = "description", columnDefinition = "varchar(2500)")
     private String description;
+    @Column(name = "code", columnDefinition = "varchar(250)")
+    private String code;
     //define other fields to collect here
 
     @PrePersist
     public void onCreate() {
         id = ObjectUtils.defaultIfNull(id, UUID.randomUUID());
         dateCreated = ObjectUtils.defaultIfNull(dateCreated, LocalDateTime.now());
+        code = UUID.randomUUID().toString().replace("-", "");
     }
 }
