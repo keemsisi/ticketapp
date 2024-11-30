50f07a976e264b342449809bdb3f9814ea538bdf
Adeshina <91054814+integral1@users.noreply.github.com>
2024-11-30 01:21:45 +0100

    updated codes

--- src/main/java/org/core/backend/ticketapp/passport/controller/ApplicationConfigController.java
+++ src/main/java/org/core/backend/ticketapp/passport/controller/ApplicationConfigController.java
@@ -2,7 +2,10 @@ package org.core.backend.ticketapp.passport.controller;
 
 import lombok.AllArgsConstructor;
 import org.core.backend.ticketapp.common.dto.GenericResponse;
+import org.core.backend.ticketapp.common.dto.PagedMapperUtil;
+import org.core.backend.ticketapp.common.dto.PagedResponse;
 import org.core.backend.ticketapp.common.enums.AccountType;
+import org.core.backend.ticketapp.marketing.dto.sponsored_offer.UpdateSponsoredOfferRequest;
 import org.core.backend.ticketapp.passport.entity.ApplicationConfig;
 import org.core.backend.ticketapp.passport.service.core.apconfig.ApplicationConfigService;
 import org.core.backend.ticketapp.passport.util.JwtTokenUtil;
@@ -12,29 +15,48 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/v1/app-configs")
 public class ApplicationConfigController {
     private final JwtTokenUtil jwtTokenUtil;
-    private final ApplicationConfigService applicationConfigService;
+    private final ApplicationConfigService service;
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> getAll(final Pageable pageable) throws Exception {
+    public ResponseEntity<GenericResponse<ApplicationConfig>> create(@Validated @RequestBody ApplicationConfig request) throws Exception {
         UserUtils.assertUserHasRole(jwtTokenUtil.getUser().getRoles(), AccountType.SUPER_ADMIN.name());
-        return new ResponseEntity<>(new GenericResponse<>("00", "Resource fetched successfully",
-                applicationConfigService.getAll(pageable)), HttpStatus.CREATED);
+        return new ResponseEntity<>(new GenericResponse<>("00", "Resource created successfully",
+                service.create(request)), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GenericResponse<ApplicationConfig>> getById(final @PathVariable UUID id) {
+        final var result = service.getById(id);
+        return ResponseEntity.ok().body(new GenericResponse<>("00", "Successfully fetched resource", result));
     }
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> create(@Validated @RequestBody ApplicationConfig request) throws Exception {
+    public ResponseEntity<GenericResponse<PagedResponse<?>>> getAll(final Pageable pageable) throws Exception {
         UserUtils.assertUserHasRole(jwtTokenUtil.getUser().getRoles(), AccountType.SUPER_ADMIN.name());
-        return new ResponseEntity<>(new GenericResponse<>("00", "Resource created successfully",
-                applicationConfigService.create(request)), HttpStatus.CREATED);
+        final var result = PagedMapperUtil.map(service.getAll(pageable));
+        return ResponseEntity.ok().body(new GenericResponse<>("00", "Successfully fetched resource", result));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GenericResponse<ApplicationConfig>> deleteById(final @PathVariable UUID id) {
+        UserUtils.assertUserHasRole(jwtTokenUtil.getUser().getRoles(), AccountType.SUPER_ADMIN.name());
+        service.delete(id);
+        return ResponseEntity.ok().body(new GenericResponse<>("00", "Successfully deleted resource!", null));
+    }
+
+    @RequestMapping(method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GenericResponse<ApplicationConfig>> update(final @RequestBody UpdateSponsoredOfferRequest request) {
+        UserUtils.assertUserHasRole(jwtTokenUtil.getUser().getRoles(), AccountType.SUPER_ADMIN.name());
+        final var result = service.update(request);
+        return ResponseEntity.ok().body(new GenericResponse<>("00", "Successfully updated resource!", result));
     }
 }
