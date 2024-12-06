e09a09874f07dfed5f3f500cae2e3173bed3ad06
Adeshina <91054814+integral1@users.noreply.github.com>
2024-12-06 21:50:20 +0100

    update on inapp follower system

--- src/main/java/org/core/backend/ticketapp/marketing/entity/InAppFollower.java
+++ src/main/java/org/core/backend/ticketapp/marketing/entity/InAppFollower.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 })
 @TypeDefs({@TypeDef(name = "JSONB", typeClass = JsonBinaryType.class)})
 public class InAppFollower extends AbstractBaseEntity {
+    // followedUserId ->> following userId
     @Column(name = "followed_user_id")
     private UUID followedUserId;
     @Column(columnDefinition = "bool default true")
