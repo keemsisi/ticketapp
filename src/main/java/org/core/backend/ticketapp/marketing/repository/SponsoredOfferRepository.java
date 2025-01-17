b38512b4f831849536d12ca76fe4d02f4dbf44bc
Adeshina <91054814+integral1@users.noreply.github.com>
2025-01-17 19:32:23 +0100

    updated codes

--- src/main/java/org/core/backend/ticketapp/marketing/repository/SponsoredOfferRepository.java
+++ src/main/java/org/core/backend/ticketapp/marketing/repository/SponsoredOfferRepository.java
@@ -13,12 +13,8 @@ import java.util.UUID;
 
 @Repository
 public interface SponsoredOfferRepository extends JpaRepository<SponsoredOffer, UUID> {
-    @Query(value = "SELECT * FROM sponsored_offer WHERE user_id = ?1 AND deleted = false AND end_date >= now() ", nativeQuery = true)
+    @Query(value = "SELECT * FROM sponsored_offer WHERE user_id = ?1 AND deleted = false ", nativeQuery = true)
     Page<SponsoredOffer> findAll(final @NotNull UUID userId, final @NotNull Pageable pageable);
-
-    @Query(value = "SELECT * FROM sponsored_offer WHERE deleted = false AND end_date >= now() ", nativeQuery = true)
-    @org.jetbrains.annotations.NotNull
-    Page<SponsoredOffer> findAll(final @NotNull @org.jetbrains.annotations.NotNull Pageable pageable);
 
     @Query(value = "SELECT * FROM sponsored_offer WHERE id = ?1 AND user_id = ?2 AND deleted = false ", nativeQuery = true)
     Optional<SponsoredOffer> findById(UUID id, UUID userId);
