419a60792ea399a312f57fba1fddfbaddea9de00
Adeshina <91054814+integral1@users.noreply.github.com>
2025-02-09 19:32:14 +0100

    used a cache to track follower counts

--- src/main/java/org/core/backend/ticketapp/marketing/controller/SocialMediaLinksAdvertisementController.java
+++ src/main/java/org/core/backend/ticketapp/marketing/controller/SocialMediaLinksAdvertisementController.java
@@ -1,5 +1,7 @@
 package org.core.backend.ticketapp.marketing.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.core.backend.ticketapp.common.dto.GenericApiResponse;
@@ -12,6 +14,7 @@ import org.core.backend.ticketapp.marketing.entity.ExternalAppFollower;
 import org.core.backend.ticketapp.marketing.entity.SocialMediaLinkAdvertisement;
 import org.core.backend.ticketapp.marketing.service.SocialMediaLinkAdvertisementService;
 import org.core.backend.ticketapp.marketing.service.impl.ExternalAppFollowerService;
+import org.core.backend.ticketapp.passport.util.ActivityLogProcessorUtils;
 import org.core.backend.ticketapp.passport.util.JwtTokenUtil;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -30,12 +33,15 @@ import java.util.UUID;
 public class SocialMediaLinksAdvertisementController {
     private final SocialMediaLinkAdvertisementService service;
     private final ExternalAppFollowerService followerService;
+    private final ActivityLogProcessorUtils activityLogProcessorUtils;
+    private final ObjectMapper objectMapper;
     private final JwtTokenUtil jwtTokenUtil;
 
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GenericApiResponse<SocialMediaLinkAdvertisement>> create(@Validated @RequestBody CreateSocialLinksRequest request) throws Exception {
-//        UserUtils.assertUserHasRole(jwtTokenUtil.getUser().getRoles(), AccountType.SUPER_ADMIN.getType());
         final var result = service.create(request);
+        activityLogProcessorUtils.processActivityLog(jwtTokenUtil.getUser().getUserId(), SocialMediaLinkAdvertisement.class.getTypeName(), null, objectMapper.writeValueAsString(result),
+                "Initiated a request to follow create social media link advert");
         return new ResponseEntity<>(new GenericApiResponse<>("00", "Social links created successfully", result), HttpStatus.CREATED);
     }
 
@@ -54,19 +60,25 @@ public class SocialMediaLinksAdvertisementController {
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GenericApiResponse<SocialMediaLinkAdvertisement>> deleteById(final @PathVariable UUID id) {
         service.delete(id);
+        activityLogProcessorUtils.processActivityLog(jwtTokenUtil.getUser().getUserId(), SocialMediaLinkAdvertisement.class.getTypeName(), null, null,
+                "Initiated a request to follow delete social media link advert");
         return ResponseEntity.ok().body(new GenericApiResponse<>("00", "Successfully deleted social link!", null));
     }
 
     @RequestMapping(method = RequestMethod.PATCH, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GenericApiResponse<SocialMediaLinkAdvertisement>> update(final @RequestBody UpdateSocialLinksRequest request) {
+    public ResponseEntity<GenericApiResponse<SocialMediaLinkAdvertisement>> update(final @RequestBody UpdateSocialLinksRequest request) throws JsonProcessingException {
         final var result = service.update(request);
+        activityLogProcessorUtils.processActivityLog(jwtTokenUtil.getUser().getUserId(), SocialMediaLinkAdvertisement.class.getTypeName(), null, objectMapper.writeValueAsString(result),
+                "Initiated a request to follow update social media link advert");
         return ResponseEntity.ok().body(new GenericApiResponse<>("00", "Successfully updated social link!", result));
     }
 
 
     @RequestMapping(value = "/follow", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<GenericApiResponse<ExternalAppFollower>> follow(final @RequestBody FollowUserSocialLinkRequest request) {
+    public ResponseEntity<GenericApiResponse<ExternalAppFollower>> follow(final @RequestBody FollowUserSocialLinkRequest request) throws JsonProcessingException {
         final var result = followerService.follow(request);
+        activityLogProcessorUtils.processActivityLog(jwtTokenUtil.getUser().getUserId(), ExternalAppFollower.class.getTypeName(), null, objectMapper.writeValueAsString(result),
+                "Initiated a request to follow a user");
         return ResponseEntity.ok().body(new GenericApiResponse<>("00", "Follow user social link was successful", result));
     }
 }
