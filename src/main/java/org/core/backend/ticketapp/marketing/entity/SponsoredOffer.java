9ad6b0faf3dd9466c3f5a20edd55b571ff19ad90
Adeshina <91054814+integral1@users.noreply.github.com>
2025-03-16 21:07:41 +0100

    add event fees calucaltuon

--- src/main/java/org/core/backend/ticketapp/marketing/entity/SponsoredOffer.java
+++ src/main/java/org/core/backend/ticketapp/marketing/entity/SponsoredOffer.java
@@ -8,10 +8,7 @@ import org.core.backend.ticketapp.common.entity.AbstractBaseEntity;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.PrePersist;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -26,24 +23,40 @@ import java.util.UUID;
 public class SponsoredOffer extends AbstractBaseEntity {
     @Column(name = "title", columnDefinition = "varchar(250)", nullable = false)
     private String title;
+
     @Column(name = "valid_from", columnDefinition = "timestamptz", nullable = false)
     private LocalDateTime validFrom;
+
     @Column(name = "valid_to", columnDefinition = "timestamptz", nullable = false)
     private LocalDateTime validTo;
+
     @Column(name = "details", nullable = false)
     private String details;
+
     @Column(name = "code", nullable = false)
     private String code;
+
     @Column(name = "image", nullable = false)
     private String image;
+
     @Column(name = "event_id")
     private UUID eventId;
+
     @Column(name = "discount")
     private Double discount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "discount_type", columnDefinition = "varchar(15) default 'PERCENTAGE'")
+    private DiscountType discountType;
 
     @PrePersist
     public void onCreate() {
         id = ObjectUtils.defaultIfNull(id, UUID.randomUUID());
         dateCreated = ObjectUtils.defaultIfNull(dateCreated, LocalDateTime.now());
+        discountType = ObjectUtils.defaultIfNull(discountType, DiscountType.PERCENTAGE);
+    }
+
+    public enum DiscountType {
+        PERCENTAGE, FIXED
     }
 }
